7fec4f8d3acaae95a006d22e0b266d535c483954
jorge228 <jorgelozano228@gmail.com>
2021-07-11 14:17:28 +0200

    show global map

--- src/app/pages/map/map.page.ts
+++ src/app/pages/map/map.page.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
+declare var mapboxgl: any;
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.page.html',
   styleUrls: ['./map.page.scss'],
 })
-export class MapPage implements OnInit {
+export class MapPage implements OnInit, AfterViewInit {
 
   lat: number;
   lng: number;
@@ -21,9 +23,14 @@ export class MapPage implements OnInit {
     this.lng = Number(geo[1]);
     console.log('lat', this.lat);
     console.log('lng', this.lng);
+  }
 
-
-
+  ngAfterViewInit(): void {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoiam9yZ2UyMjgiLCJhIjoiY2tucmFuandwMGt6ajJvcGZlN3EzZmVmaSJ9.JonXlXBZXy4EGJy9ovU2Xw';
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11'
+    });
   }
 
 }
